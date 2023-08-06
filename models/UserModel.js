07f2ed1ce2183535d324b6c2867346e4d27e22a9
Marion Robert <mrobert148@gmail.com>
2023-08-06 13:28:49 +0200

    save and login user ok (models & routes)

--- models/UserModel.js
+++ models/UserModel.js
@@ -7,4 +7,53 @@ module.exports = (_db)=>{
 }
 
 class UserModel {
+
+  //sauvegarde d'un utilisateur
+  static async saveOneUser(req){
+    // console.log(req.body)
+    //on hash le password
+    return bcrypt.hash(req.body.password, saltRounds)
+    .then((hashPassword)=>{
+      //requète sql
+      let sql = "INSERT INTO users (email, password, firstName, lastName, phone, role, points, creationTimestamp, accountIsConfirmed) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"
+      return db.query(sql, [req.body.email, hashPassword, req.body.firstName, req.body.lastName, req.body.phone, "user", 0, new Date(), "no"])
+      .then((res)=>{
+        console.log("res requête sql saveOneUser" + res)
+        res.key_id = key_id
+        return res
+      })
+      .catch((err)=>{
+        console.log("erreur requête sql saveOneUser" + err)
+        return err
+      })
+    })
+    .catch((err)=>{
+      return err
+    })
+	}
+
+  //récupération d'un utilisateur en fonction de son mail
+  static getUserByEmail(email){
+    return db.query("SELECT * FROM users WHERE email = ?", [email])
+    .then((res)=>{
+        return res
+    })
+    .catch((err)=>{
+        return err
+    })
+  }
+
+  static updateConnexion(id){
+    return db.query("UPDATE users SET connexionTimestamp = NOW() WHERE id = ?", [id])
+    .then((res)=>{
+        return res
+    })
+    .catch((err)=>{
+        return err
+    })
+
 }
+}
+
+
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWQiOjYsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkxMzIxMjgwfQ.jfSbUBAEpq0eAbkih6SijGkN_DKQ9rewEnIYiL3wENY"
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,80 @@ const withAuth = require("../withAuth")
 
 module.exports = (app, db) => {
   const userModel = require("../models/UserModel")(db)
+
+  //route d'enregistrement d'un utilisateur
+  app.post("/api/v1/user/register", async(req,res, next)=>{
+    let check = await userModel.getUserByEmail(req.body.email)
+    if (check.code){
+      res.json({status: 500, msg: "La vérification de l'existence du mail n'a pas pu aboutir"})
+    } else {
+      if (check.length > 0) {
+        res.json({status: 401, msg: "Un compte existe déjà avec cette adresse email. Veuillez vous connecter."})
+      } else {
+        let user = await userModel.saveOneUser(req)
+        if (user.code){
+          res.json({status: 500, msg: "Erreur dans la création d'un nouvel utilisateur."})
+        } else {
+          res.json({status: 200, msg: "Le compte utilisateur a bien été créé."})
+        }
+      }
+    }
+  })
+
+  //route de connexion d'un utilisateur (c'est ici qu'on va créer le token et l'envoyer vers le front)
+  app.post('/api/v1/user/login', async (req, res, next)=>{
+    if(req.body.email === ""){
+        res.json({status: 401, msg: "Entrez un email..."})
+    } else {
+        //on check si il existe un utilisateur dans la bdd avec un mail correspondant
+        let user = await userModel.getUserByEmail(req.body.email)
+        if(user.code){
+            res.json({status: 500, msg: "Erreur vérification email.", err: user})
+        }else{
+            //si il n'existe pas
+            if(user.length === 0){
+                res.json({status:404, msg: "Pas d'utilisateur correspondant à ce mail."})
+            } else {
+                //la bdd a retourné un objet d'utilisateur pour ce mail
+                //on compare les password avec bcrypt
+                let same = await bcrypt.compare(req.body.password, user[0].password)
+                //si c'est true, les mdp sont identiques
+                if(same){
+                    //on va créer le payload du token, dans ce payload on stock les valeurs qu'on va glisser dans le token (attention jamais d'infos craignos)
+                    const payload = {email: req.body.email, id: user[0].id, role: user[0].role}
+                    //on crée notre token avec sa signature (secret)
+                    const token = jwt.sign(payload, secret)
+                    let connect = await userModel.updateConnexion(user[0].id)
+                    if(connect.code){
+                        res.json({status: 500, errr: connect})
+                    } else {
+                        res.json({status: 200, token: token, user: user[0]})
+                    }
+                }else{
+                    res.json({status: 401, error: "Votre mot de passe est incorrect!"})
+                }
+            }
+        }
+    }
+  })
+
+  // //route de modification des utilisateurs
+  // app.put("/api/v1/user/update/:id", withAuth, async(req, res, next)=>{
+  //   if (isNaN(req.params.id)){
+  //     res.json({status: 500, msg: "L'id renseigné n'est pas un nombre"})
+  //   } else {
+  //     let user = await userModel.updateUser(req, req.params.id)
+  //     if (user.code){
+  //       res.json({status: 500, msg: "Les informations de l'utilisateur n'ont pas été mises à jour."})
+  //     } else {
+  //       //mon profil est modifié je renvoie les infos du profil mises à jour vers le front (pour que redux mette à jour immédiatement les infos d'utilisateur connecté)
+  //       let newUser = await userModel.getOneUser(req.params.id)
+  //       if(newUser.code){
+  //           res.json({status: 500, msg: "Un problème est survenu.", err: newUser})
+  //       } else {
+  //           res.json({status: 200, result: user, newUser: newUser[0], msg: "Les informations de l'utilisateur ont bien été mises à jour." })
+  //       }
+  //     }
+  //   }
+  // })
 }
